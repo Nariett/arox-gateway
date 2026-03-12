11e1101e1f9c97fd17495991d98120748df66839
Sasha <samsiki50@gmail.com>
2026-03-13 00:31:12 +0300

    fix server

--- rest/server.go
+++ rest/server.go
@@ -1,19 +1,39 @@
 package rest
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/Nariett/arox-pkg/config"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/fx"
 )
 
-func StartHttpServer(cfg *config.Config, r chi.Router) {
-	log.Printf("Starting server at port :%s\n", cfg.Port)
-	port := fmt.Sprintf(":%s", cfg.Port)
-	err := http.ListenAndServe(port, r)
-	if err != nil {
-		log.Println("Error starting the server:", err)
+func StartHttpServer(lc fx.Lifecycle, cfg *config.Config, r chi.Router, shutdowner fx.Shutdowner) {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", cfg.Port),
+		Handler: r,
 	}
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			log.Printf("Starting HTTP server at :%s\n", cfg.Port)
+			go func() {
+				if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+					log.Printf("HTTP server error: %v", err)
+					_ = shutdowner.Shutdown()
+				}
+			}()
+			return nil
+		},
+
+		OnStop: func(ctx context.Context) error {
+			log.Println("Shutting down HTTP server...")
+
+			return srv.Shutdown(ctx)
+		},
+	})
 }
--- rpc/server.go
+++ rpc/server.go
@@ -1,21 +1,36 @@
 package rpc
 
 import (
+	"context"
 	"fmt"
 	"log"
 
 	"github.com/Nariett/arox-pkg/config"
 	proto "github.com/Nariett/arox-pkg/grpc/pb/products"
+	"go.uber.org/fx"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func ListenServer(cfg *config.Config) proto.ProductsServiceClient {
+func ListenServer(lc fx.Lifecycle, cfg *config.Config) (proto.ProductsServiceClient, error) {
 	connStr := fmt.Sprintf("%s:%s", cfg.Host, cfg.ProductsPort)
-	conn, err := grpc.Dial(connStr, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Error with grpc connection: %v", err)
-	}
-	client := proto.NewProductsServiceClient(conn)
 
-	return client
+	conn, err := grpc.NewClient(
+		connStr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("grpc dial %s: %w", connStr, err)
+	}
+
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			log.Println("Closing gRPC connection")
+
+			return conn.Close()
+		},
+	})
+
+	return proto.NewProductsServiceClient(conn), nil
 }
